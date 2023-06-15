b0924ead7679f4cfa55e6ac17a3a30a46218bef6
Raul Belloso M <raul.belloso.m@gmail.com>
2023-06-14 19:12:00 -0600

    redirects

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Layout from "../components/Layout";
 import Tratamientos from "../pages/tratamientos/Tratamientos";

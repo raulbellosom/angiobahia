705f597199c0a36663703fb65896d7ea8443ccfd
Raul Belloso M <raul.belloso.m@gmail.com>
2023-06-16 12:16:45 -0600

    dropdown navbar fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -75,7 +75,7 @@ const Nav = () => {
             </span>
           </button>
           {isOpen && (
-            <div className="absolute right-0 w-40 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none">
+            <div className="absolute left-0 md:right-0 w-40 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none">
               <div className="py-1">
                 <a
                   href="/trayectoria"
